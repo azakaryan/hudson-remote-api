448446218c7f9fdb970690f78f33a4d84576c8e7
Armen <armen0089@gmail.com>
2017-07-31 19:09:13 +0400

    renamed directory

--- lib/jenkins-remote-api_1.rb
+++ lib/jenkins-remote-api_1.rb
@@ -1,5 +1,5 @@
 require 'uri'
 require 'net/http'
-require './lib/hudson-remote-api/client.rb'
+require './lib/jenkins-remote-api_1/client.rb'
 
-Dir[File.dirname(__FILE__) + '/hudson-remote-api/**/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/jenkins-remote-api_1/**/*.rb'].each {|file| require file }
