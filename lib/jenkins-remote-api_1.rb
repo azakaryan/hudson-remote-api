43593f3eab8b961006309ab34a8da594d28817d9
Armen <armen0089@gmail.com>
2017-07-31 19:50:30 +0400

    changed relative part

--- lib/jenkins-remote-api_1.rb
+++ lib/jenkins-remote-api_1.rb
@@ -1,5 +1,5 @@
 require 'uri'
 require 'net/http'
-require './lib/jenkins-remote-api_1/client.rb'
+require_relative './jenkins-remote-api_1/client.rb'
 
 Dir[File.dirname(__FILE__) + '/jenkins-remote-api_1/**/*.rb'].each {|file| require file }
